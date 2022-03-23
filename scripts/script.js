f932a503bab24a75dc6f4ed8119c626b30ee8ab7
DrakZxho <101566599+DrakZxho@users.noreply.github.com>
2022-03-23 21:58:44 +0100

    js test

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+let lang="es";
+var i=0;
+var dato;
+var atrEN=new Array("VIT", "STR", "DEF", "DEX", "SPR", "LCK", "RES", "MAG");
+var atrES=new Array("VIT", "FUE", "DEF", "DES", "ESP", "SUE", "RES", "MAG");
+var atrLN;
--- scripts/weapons.js
+++ scripts/weapons.js
@@ -1,10 +1,3 @@
-let lang="es";
-var i=0;
-var dato;
-var atrEN=new Array("VIT", "STR", "DEF", "DEX", "SPR", "LCK", "RES", "MAG");
-var atrES=new Array("VIT", "FUE", "DEF", "DES", "ESP", "SUE", "RES", "MAG");
-var atrLN;
-
 function createWeaponChart(dato){
 
     $("#chart").html("");
