02cad519e251db6271258a05d46dcb1cd95bd6dc
DrakZxho <101566599+DrakZxho@users.noreply.github.com>
2022-03-24 00:35:57 +0100

    003524032022

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+function arrowMove(e){
+    if(e.key=="ArrowUp"){
+        window.location.replace("https://drakzxho.github.io/trial/pages/foes.html");
+    }else if(e.key=="ArrowDown"){
+        window.location.replace("https://drakzxho.github.io/trial/pages/weapons.html");
+    }
+}//arrowMove
